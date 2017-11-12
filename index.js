3b8bd8ff2c14fa8b68652cce14d2d077248eabba
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-12 23:41:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  return drivers.map(function(driver){
+    return driver.toLowerCase()
+  })
+  
+}
+// 
+// var numbers = [1, 5, 10, 15];
+// var doubles = numbers.map(function(x) {
+//    return x * 2;
+// });
