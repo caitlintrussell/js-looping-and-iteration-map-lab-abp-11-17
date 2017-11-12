4a8d5d51b45e3077f4b9f2fbdb955883374d1616
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-12 23:55:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ function lowerCaseDrivers(drivers){
   return drivers.map(function(driver){
     return driver.toLowerCase()
   })
-  
+
 }
-// 
+function nameToAttributes(drivers){
+  return drivers.map(function(driver){
+    return [driver.firstName]
+  })
+}
+//
 // var numbers = [1, 5, 10, 15];
 // var doubles = numbers.map(function(x) {
 //    return x * 2;
 // });
+function splitNames(fullName){
+  let firstName = fullName.split(' ')[0];
+  let lastName = fullName.split(' ')[2];
+  return `First Name: ${firstName}, Last Name: ${lastName}`
+}
