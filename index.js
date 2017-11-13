a493257c88efb07bcd05e385afb2f121b0e94d4f
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-13 00:05:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function lowerCaseDrivers(drivers){
 function nameToAttributes(names){
   return names.map(function(name){
     let fName = name.split(' ')[0];
-    let lName = name.split(' ')[2];
+    let lName = name.split(' ')[1];
     return {firstName: fName, lastName: lName}
   })
 }
